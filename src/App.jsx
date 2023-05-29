39729ab50e7bc8b54b589fb915e3b7f1f95927ed
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-30 01:59:59 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // import Layout from './Layout'
 // import Chain from './pages/Chain'
-// import Chart from './pages/Chart'
-// import Proposition from './pages/Proposition'
+import Chart from './pages/Chart'
+import Proposition from './pages/Proposition'
 // import Team from './pages/Team'
 // import Home from './sections/Home'
+import Intro from './pages/Intro'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import ThankYou from "./pages/ThankYou"
 
@@ -12,10 +19,20 @@ function App() {
 
   return (
     <>
+     <Router>
+     <Routes>
+
+     <Route path="/" element={<Intro/>} />
+     <Route path="/proposition" element={<Proposition/>} />
+     <Route path="/chart" element={<Chart/>} />
+
+
+     </Routes>
+     </Router>
     {/* <Team/> */}
     {/* <Chart/> */}
     {/* <Chain/> */}
-    <ThankYou/>
+ 
     
        
     </>
