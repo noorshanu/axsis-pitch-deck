3b5019324f80ea19edcc1d10b345167f0b87fda3
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-30 01:35:13 +0530

    Update ThankYou.jsx

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -7,7 +7,7 @@ function ThankYou() {
         <div className='text-center mx-auto'>
             <img src="assets/thank.svg" alt="" />
             <h1 className='font-arc text-6xl text-white font-bold mt-8 mb-5'>THANK YOU!</h1>
-            <div className='flex justify-center gap-4 mt-8'>
+            <div className='flex justify-center gap-4 mt-11'>
                 <img src="assets/btn.png" alt="" />
                 <img src="assets/btn2.png" alt="" />
 
